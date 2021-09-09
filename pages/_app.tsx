5f16db8a39e24b5a3bf0f03f0d43910872192c13
Dennis Chen <dchen@dennisc.net>
2021-09-09 15:43:18 -0700

    gray bckg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
         <title>MAST</title>
         <link rel="shortcut icon" type="image/x-icon" href="masticon.svg" />
       </Head>
-      <div className="flex justify-center">
+      <div className="min-h-screen bg-gray-100 flex justify-center">
         <div className="mx-12 my-12 w-full max-w-screen-lg space-y-2">
           <Component {...pageProps} />
         </div>
