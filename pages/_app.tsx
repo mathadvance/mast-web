18a4d2f2c45b86e20d84ab81e9be53626dce3923
Dennis Chen <dchen@dennisc.net>
2021-09-09 15:39:36 -0700

    Make app.tsx look a little better

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import "styles/global.css";
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+      <Head>
+        <title>MAST</title>
+        <link rel="shortcut icon" type="image/x-icon" href="masticon.svg" />
+      </Head>
+      <div className="flex justify-center">
+        <div className="mx-12 my-12 w-full max-w-screen-lg space-y-2">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default MyApp;
