f6170a03a8098c1717da9e63609fd17195c8fd1b
Dennis Chen <dchen@dennisc.net>
2021-09-10 15:21:19 -0700

    setup bars in app

---
+++ components/SideBar.tsx
@@ -0,0 +1,17 @@
+function SideRow({ link, label, icon }) {
+    return (
+        <div className="">
+
+        </div>
+    );
+};
+
+function SideBar() {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SideBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import "styles/global.css";
 import Head from "next/head"
+import { useRouter } from "next/router";
+
+import "@/styles/global.css";
+import TopBar from "@/components/TopBar";
+import SideBar from "@/components/SideBar";
+
+const emptyPages = ["login", "signup"];
+// pages where components
+// like NavBar and SideBar
 
 function MyApp({ Component, pageProps }) {
+  const { asPath } = useRouter();
+  const paths = asPath.split("/");
+  const lastPath = paths[paths.length - 1];
   return (
     <>
       <Head>
@@ -9,10 +20,27 @@ function MyApp({ Component, pageProps }) {
         <link rel="shortcut icon" type="image/x-icon" href="masticon.svg" />
       </Head>
       <div className="min-h-screen bg-gray-100 flex justify-center">
-        <div className="mx-12 my-12 w-full max-w-screen-lg">
-          <div className="space-y-2">
-            <Component {...pageProps} />
-          </div>
+        <div className="px-12 py-6 w-full max-w-screen-lg">
+          {emptyPages.indexOf(lastPath) > -1 ?
+            (
+              <div className="p-6 bg-white rounded-xl shadow-lg space-y-2">
+                <Component {...pageProps} />
+              </div>
+            ) : (
+              <>
+                <TopBar />
+                <div className="my-8" />
+                <div className="flex gap-x-10">
+                  <div className="p-6 bg-white rounded-xl shadow-lg space-y-2">
+                    <Component {...pageProps} />
+                  </div>
+                  <div className="w-48 sm:w-96">
+                    <SideBar />
+                  </div>
+                </div>
+              </>
+            )
+          }
         </div>
       </div>
     </>
