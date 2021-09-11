5dcdfad85eed70c0ff31ce0b2f609c477037bff9
Dennis Chen <proofprogram@gmail.com>
2021-09-11 13:53:13 -0700

    primitive topbar, no logo yet

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,30 +1,30 @@
 import NextLink from "next/link";
 
 function TopBar() {
-    return (
-        <div className="w-full max-w-screen-lg mx-auto flex justify-between p-2 border-b-2 border-gray-300 text-lg">
-            <div>
-                MAST
-            </div>
-            <div className="flex gap-x-1.5">
-                <div>
-                    <NextLink href="signup">
-                        <a className="text-blue-600 hover:text-blue-500 hover:underline">
-                            Sign Up
-                        </a>
-                    </NextLink>
-                </div>
-                <div>|</div>
-                <div>
-                    <NextLink href="login">
-                        <a className="text-blue-600 hover:text-blue-500 hover:underline">
-                            Log In
-                        </a>
-                    </NextLink>
-                </div>
-            </div>
+  return (
+    <div className="w-full max-w-screen-lg mx-auto flex justify-between p-2 border-b-2 border-gray-300 text-lg">
+      <NextLink href="/">
+        <a>MAST</a>
+      </NextLink>
+      <div className="flex gap-x-1.5">
+        <div>
+          <NextLink href="signup">
+            <a className="text-blue-600 hover:text-blue-500 hover:underline">
+              Sign Up
+            </a>
+          </NextLink>
         </div>
-    );
-};
+        <div className="text-gray-400">|</div>
+        <div>
+          <NextLink href="login">
+            <a className="text-blue-600 hover:text-blue-500 hover:underline">
+              Log In
+            </a>
+          </NextLink>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default TopBar;
