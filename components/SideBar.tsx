17487c49cba47ce5c0b093e030fbb881f118c27b
Dennis Chen <proofprogram@gmail.com>
2021-09-11 15:04:12 -0700

    this is kinda cursed I hate it

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,17 +1,29 @@
-function SideRow({ link, label, icon }) {
-    return (
-        <div className="">
+import NextLink from "next/link";
 
-        </div>
-    );
-};
+import { AiOutlineInfoCircle, AiOutlineDollar } from "react-icons/ai";
+import { BsPersonFill } from "react-icons/bs";
+import { IoMdPaperPlane } from "react-icons/io";
+
+function SideRow({ link, label, icon }) {
+  return (
+    <NextLink href={link}>
+      <a className="flex gap-x-2 items-center text-pink-700 hover:text-pink-600 hover:underline">
+        <div className="text-lg">{icon}</div>
+        <div>{label}</div>
+      </a>
+    </NextLink>
+  );
+}
 
 function SideBar() {
-    return (
-        <div>
-
-        </div>
-    );
-};
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-1 gap-y-1.5 bg-pink-50 rounded-xl border border-pink-500 shadow-lg p-4">
+      <SideRow link="/about" label="About" icon={<AiOutlineInfoCircle />} />
+      <SideRow link="/staff" label="Staff" icon={<BsPersonFill />} />
+      <SideRow link="/apply" label="Apply" icon={<IoMdPaperPlane />} />
+      <SideRow link="/payment" label="Payment" icon={<AiOutlineDollar />} />
+    </div>
+  );
+}
 
 export default SideBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 import "@/styles/global.css";
@@ -21,30 +21,28 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <div className="min-h-screen bg-gray-100 flex justify-center">
         <div className="px-12 py-6 w-full max-w-screen-lg">
-          {emptyPages.indexOf(lastPath) > -1 ?
-            (
-              <div className="p-6 bg-white rounded-xl shadow-lg space-y-2">
-                <Component {...pageProps} />
-              </div>
-            ) : (
-              <>
-                <TopBar />
-                <div className="my-8" />
-                <div className="flex gap-x-10">
-                  <div className="p-6 bg-white rounded-xl shadow-lg space-y-2">
-                    <Component {...pageProps} />
-                  </div>
-                  <div className="w-48 sm:w-96">
-                    <SideBar />
-                  </div>
+          {emptyPages.indexOf(lastPath) > -1 ? (
+            <div className="p-6 bg-white rounded-xl shadow-lg space-y-2">
+              <Component {...pageProps} />
+            </div>
+          ) : (
+            <>
+              <TopBar />
+              <div className="my-8" />
+              <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-3 gap-x-10">
+                <div className="order-last sm:order-first sm:col-span-2 p-6 bg-white rounded-xl shadow-lg space-y-2">
+                  <Component {...pageProps} />
                 </div>
-              </>
-            )
-          }
+                <div>
+                  <SideBar />
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
   );
-};
+}
 
 export default MyApp;
