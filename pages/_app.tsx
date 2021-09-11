0b8ba836e43dabff87878106b23c36429ad4fe40
Dennis Chen <proofprogram@gmail.com>
2021-09-11 15:10:17 -0700

    More cursed behavior to make stuff look better

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,8 +29,8 @@ function MyApp({ Component, pageProps }) {
             <>
               <TopBar />
               <div className="my-8" />
-              <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-3 gap-x-10">
-                <div className="order-last sm:order-first sm:col-span-2 p-6 bg-white rounded-xl shadow-lg space-y-2">
+              <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-3 md:grid-cols-4 gap-x-10">
+                <div className="order-last sm:order-first sm:col-span-2 md:col-span-3 p-6 bg-white rounded-xl shadow-lg space-y-2">
                   <Component {...pageProps} />
                 </div>
                 <div>
