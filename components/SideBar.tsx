df150985e58e12220d07bbd6f3638df6000795c9
Dennis Chen <proofprogram@gmail.com>
2021-09-11 15:35:27 -0700

    fill in content and just use fa for consistency

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,10 +1,22 @@
 import NextLink from "next/link";
 
-import { AiOutlineInfoCircle, AiOutlineDollar } from "react-icons/ai";
-import { BsPersonFill } from "react-icons/bs";
-import { IoMdPaperPlane } from "react-icons/io";
+import {
+  FaHome,
+  FaCog,
+  FaInfo,
+  FaQuestion,
+  FaDollarSign,
+  FaPaperPlane,
+  FaUser,
+  FaFileAlt,
+  FaDoorOpen,
+} from "react-icons/fa";
 
-function SideRow({ link, label, icon }) {
+function SideRow({ needsUser = false, link, label, icon }) {
+  if (needsUser) {
+    // do some checking
+    return null;
+  }
   return (
     <NextLink href={link}>
       <a className="flex gap-x-2 items-center text-pink-700 hover:text-pink-600 hover:underline">
@@ -17,11 +29,21 @@ function SideRow({ link, label, icon }) {
 
 function SideBar() {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-1 gap-y-1.5 bg-pink-50 rounded-xl border border-pink-500 shadow-lg p-4">
-      <SideRow link="/about" label="About" icon={<AiOutlineInfoCircle />} />
-      <SideRow link="/staff" label="Staff" icon={<BsPersonFill />} />
-      <SideRow link="/apply" label="Apply" icon={<IoMdPaperPlane />} />
-      <SideRow link="/payment" label="Payment" icon={<AiOutlineDollar />} />
+    <div className="grid grid-cols-2 sm:grid-cols-1 gap-y-1.5 bg-pink-50 rounded-xl border border-pink-500 p-4">
+      <SideRow needsUser link="/home" label="Home" icon={<FaHome />} />
+      <SideRow needsUser link="/settings" label="Settings" icon={<FaCog />} />
+      <SideRow link="/about" label="About" icon={<FaInfo />} />
+      <SideRow link="/staff" label="Staff" icon={<FaUser />} />
+      <SideRow link="/apply" label="Apply" icon={<FaPaperPlane />} />
+      <SideRow link="/payment" label="Payment" icon={<FaDollarSign />} />
+      <SideRow link="/resources" label="Resources" icon={<FaFileAlt />} />
+      <SideRow link="/faq" label="FAQ" icon={<FaQuestion />} />
+      <SideRow
+        needsUser
+        link="/signout"
+        label="Sign Out"
+        icon={<FaDoorOpen />}
+      />
     </div>
   );
 }
