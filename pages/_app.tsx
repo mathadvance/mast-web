63adbf026a84b9ae8e3d8b1fe4cadb708e1e9575
Dennis Chen <proofprogram@gmail.com>
2021-09-12 22:56:52 -0700

    themeprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+import ThemeProvider from "@/utils/ThemeProvider";
+
 import "@/styles/global.css";
 import TopBar from "@/components/TopBar";
 import SideBar from "@/components/SideBar";
@@ -13,8 +15,9 @@ function MyApp({ Component, pageProps }) {
   const { asPath } = useRouter();
   const paths = asPath.split("/");
   const lastPath = paths[paths.length - 1];
+
   return (
-    <>
+    <ThemeProvider>
       <Head>
         <title>MAST</title>
         <link rel="shortcut icon" type="image/x-icon" href="masticon.svg" />
@@ -22,16 +25,14 @@ function MyApp({ Component, pageProps }) {
       <div className="min-h-screen bg-gray-100 flex justify-center">
         <div className="px-12 py-6 w-full max-w-screen-lg">
           {emptyPages.indexOf(lastPath) > -1 ? (
-            <div className="p-6 bg-white rounded-xl shadow-lg space-y-2">
-              <Component {...pageProps} />
-            </div>
+            <Component {...pageProps} />
           ) : (
             <>
               <TopBar />
               <div className="my-8" />
               <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-3 md:grid-cols-4 gap-x-10">
                 <div className="order-last sm:order-first sm:col-span-2 md:col-span-3">
-                  <div className="p-6 bg-white rounded-xl shadow-lg space-y-2">
+                  <div className="p-6 bg-white dark:bg-black rounded-xl shadow-lg space-y-2">
                     <Component {...pageProps} />
                   </div>
                 </div>
@@ -43,7 +44,7 @@ function MyApp({ Component, pageProps }) {
           )}
         </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
