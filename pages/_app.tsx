0ae0a7c5804db1cafff5f454c89dbfceaa0d9c16
gaussianprime <wdai418@gmail.com>
2021-09-15 23:02:39 -0500

    adjustment to footer placement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,14 +25,14 @@ function MyApp({ Component, pageProps }) {
           <link rel="shortcut icon" type="image/x-icon" href="masticon.svg" />
         </Head>
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex justify-center">
-          <div className="px-12 py-6 w-full max-w-screen-lg md:max-w-screen-xl xl:max-w-max">
+          <div className="flex-grow px-12 py-6 w-full max-w-screen-lg md:max-w-screen-xl xl:max-w-max">
             {emptyPages.indexOf(lastPath) > -1 ? (
               <Component {...pageProps} />
             ) : (
               <>
                 <TopBar />
                 <div className="my-8" />
-                <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-3 md:grid-cols-4 gap-x-10">
+                <div className="flex-grow grid grid-cols-1 gap-y-8 sm:grid-cols-3 md:grid-cols-4 gap-x-10">
                   <div className="order-last sm:order-first sm:col-span-2 md:col-span-3">
                     <div className="p-6 bg-white dark:bg-gray-800 rounded-xl shadow-lg space-y-2">
                       <Component {...pageProps} />
@@ -44,7 +44,7 @@ function MyApp({ Component, pageProps }) {
                 </div>
               </>
             )}
-            <div className = "absolute inset-x-0 bottom-0 p-12">
+            <div className = "bg-gray-100 inset-x-0 bottom-0 p-6">
             <Footer/>
             </div>
           </div>
