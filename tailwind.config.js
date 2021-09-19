daf175547effb0ca02554150812ddccc5e58dde0
Dennis Chen <proofprogram@gmail.com>
2021-09-18 22:43:00 -0700

    Overwrite screens to include widephone

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
   ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      // completely replace
+      // extend doesn't work because otherwise, widephone is applied last
+      // don't include lg xl or 2xl because they aren't used
+      widephone: "480px",
+      sm: "640px",
+      md: "768px",
+    },
   },
   variants: {
     extend: {},
