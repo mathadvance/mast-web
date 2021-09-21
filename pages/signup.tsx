4cf98048e78fba15339ea60cb7cda22d01a69020
Dennis Chen <dchen@dennisc.net>
2021-09-20 20:24:32 -0700

    temp

---
+++ pages/signup.tsx
@@ -0,0 +1,11 @@
+function Signup() {
+    return (
+        <div className="flex w-full min-h-screen justify-center items-center">
+            <div className="bg-gray-100 w-full max-w-lg">
+                div
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
