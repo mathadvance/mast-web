3bda1fd827e163225cbfb8c507c3c61f9ccb71f6
Dennis Chen <proofprogram@gmail.com>
2021-09-21 22:57:18 -0700

    Signup form UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,7 +32,7 @@ function MyApp({ Component, pageProps }) {
               <div className="my-8" />
               <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-3 md:grid-cols-4 gap-x-10">
                 <div className="order-last sm:order-first sm:col-span-2 md:col-span-3">
-                  <div className="p-6 bg-white dark:bg-gray-800 rounded-xl shadow-lg space-y-2">
+                  <div className="p-6 bg-gray-50 dark:bg-gray-800 rounded-xl shadow-lg space-y-2">
                     <Component {...pageProps} />
                   </div>
                 </div>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,25 +1,26 @@
-import { logoPath } from "@/utils/paths"
 import NextLink from "next/link";
+import { FormBox, FormInput } from "@/components/FormComponents";
 
 function Signup() {
-    return (
-        <div className="flex w-full min-h-screen justify-center items-center">
-            <div className="bg-white w-full max-w-lg rounded-xl shadow-lg">
-                <div className="px-7 pt-7 pb-5 border-b border-gray-200">
-                    <NextLink href="/">
-                        <a>
-                            <img src={logoPath} className="h-14" />
-                        </a>
-                    </NextLink>
-                </div>
-                <div className="px-7 pt-5 pb-7">
-                    <h1 className="font-normal">
-                        Sign Up
-                    </h1>
-                </div>
-            </div>
-        </div>
-    );
-};
+  return (
+    <FormBox>
+      <h1 className="font-normal">Sign Up</h1>
+      <FormInput placeholder="Email Address" />
+      <FormInput placeholder="Name" />
+      <FormInput placeholder="Username" />
+      <FormInput placeholder="Graduation Year" />
+      <FormInput placeholder="Password" />
+      <FormInput placeholder="Confirm Password" />
+      <div>
+        {`Have an account already? `}
+        <NextLink href="login">
+          <a className="text-blue-600 hover:text-blue-500 dark:text-blue-500 dark:hover:text-blue-400 hover:underline">
+            Log in.
+          </a>
+        </NextLink>
+      </div>
+    </FormBox>
+  );
+}
 
 export default Signup;
