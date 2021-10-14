55b588631388d0741b6130bf2f1db0a0d1624c0d
Dennis Chen <proofprogram@gmail.com>
2021-10-13 21:58:59 -0700

    call updateUser after verifying email

--- pages/profile/verify_email.tsx
+++ pages/profile/verify_email.tsx
@@ -1,53 +1,60 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { useAuth } from "@/contexts/AuthProvider";
 
 export default function verify_email() {
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
-    const [verificationSucceeded, setVerificationSucceded] = useState(false)
-    let key;
+  const { user, setUser } = useAuth();
 
-    useEffect(() => {
-        if (!router.isReady) {
-            return;
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [verificationSucceeded, setVerificationSucceded] = useState(false);
+  let key;
+
+  useEffect(() => {
+    if (!router.isReady) {
+      return;
+    }
+    key = router.query.key || {};
+    (async () => {
+      if (key) {
+        const res = await fetch("/api/email/verify", {
+          method: "POST",
+          body: key,
+        });
+        const status = await res.status;
+        if (status < 300) {
+          setVerificationSucceded(true);
         }
-        key = router.query.key || {};
-        (async () => {
-            if (key) {
-                const res = await fetch("/api/email/verify", {
-                    method: "POST",
-                    body: key
-                })
-                const status = await res.status;
-                if (status < 300) {
-                    setVerificationSucceded(true);
-                }
-            }
-            setLoading(false);
-        })();
-    }, [router.isReady]);
+      }
+      setLoading(false);
+    })();
+  }, [router.isReady]);
 
-    if (loading) {
-        return null;
-    }
+  if (loading) {
+    return null;
+  }
 
-    if (verificationSucceeded) {
-        return (
-            <>
-                <h1>
-                    Email Verified
-                </h1>
-                <p>
-                    Head over to <a href="/home">your homepage</a>.
-                </p>
-            </>
-        )
-    }
-
+  if (verificationSucceeded) {
+    const updatedUser = user;
+    updatedUser.power = 1;
+    setUser(updatedUser);
     return (
-        <>
-            <h1>Email Validation Error</h1>
-            <p>The verification key is either malformed, expired, or already has been used.</p>
-        </>
+      <>
+        <h1>Email Verified</h1>
+        <p>
+          Head over to <a href="/home">your homepage</a>.
+        </p>
+      </>
     );
+  }
+
+  return (
+    <>
+      <h1>Email Validation Error</h1>
+      <p>
+        The verification key is either malformed, expired, or already has been
+        used.
+      </p>
+    </>
+  );
 }
